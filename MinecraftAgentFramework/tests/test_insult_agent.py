a339ebcb328c7394d9a2179073d23d4b8409ac5a
Servet B. Arvas <119882291+avarebiri@users.noreply.github.com>
2025-01-30 17:52:06 +0300

    Update test_insult_agent.py

--- MinecraftAgentFramework/tests/test_insult_agent.py
+++ MinecraftAgentFramework/tests/test_insult_agent.py
@@ -1,4 +1,8 @@
 import unittest
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from agents.insult_agent import InsultAgent
 
 class TestInsultAgent(unittest.TestCase):
