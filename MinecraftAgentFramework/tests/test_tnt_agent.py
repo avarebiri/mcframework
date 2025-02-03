42c63064402a51a4173b674ba7013550bde20578
Servet B. Arvas <119882291+avarebiri@users.noreply.github.com>
2025-02-03 19:10:47 +0300

    Update test_tnt_agent.py

--- MinecraftAgentFramework/tests/test_tnt_agent.py
+++ MinecraftAgentFramework/tests/test_tnt_agent.py
@@ -1,3 +1,7 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import unittest
 from unittest.mock import MagicMock, patch
 from agents.tnt_agent import TNTAgent
